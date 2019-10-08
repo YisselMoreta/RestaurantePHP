b2ce8f7626ea9542dae22be482eaf7826ac7b84a
Laura <luaana788@gmail.com>
2019-10-08 20:42:18 +0200

    inicio de maquetacion

--- Frontend/src/app/app.module.ts
+++ Frontend/src/app/app.module.ts
@@ -1,11 +1,8 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-
-
 import { RegistroAlmacenComponent } from './components/registro-almacen/registro-almacen.component';
 import { ProductoListaComponent } from './components/producto/producto-lista.component';
 import { HttpClientModule } from '@angular/common/http';
